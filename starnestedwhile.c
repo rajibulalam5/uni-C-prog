9b1f389d1deb524c33ba425615bf5deaec14c773
Rajibul Alam <alamsrv@gmail.com>
2025-01-07 23:54:20 +0600

    Add files via upload

---
+++ starnestedwhile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int r =1 ;
+
+    while(r<=5){
+            int c=1;
+
+        while(c<=r){
+            printf("* ");
+            c++;
+            }
+            printf("\n");
+            r++;
+
+    }
+}
---
+++ whileloopoddoreven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int i=1;
+
+    while(i <= 100){
+        if (i % 2 == 0) 
+        {
+            printf("%d is even\n", i);
+        }
+        else{
+            printf("%d is odd\n", i);
+        }
+        i++;
+        }
+    }
